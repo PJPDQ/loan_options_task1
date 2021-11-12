caa978ea5282f98f22a766f65883fb9f48402175
DickySG <june_dicky@hotmail.com>
2021-11-12 13:11:59 +1000

    added material-ui package for table. potential bug, await axios fetches duplicate elements.

--- react-api-task1/src/components/Tables.js
+++ react-api-task1/src/components/Tables.js
@@ -1,6 +1,33 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import * as ReactBootstrap from 'react-bootstrap'
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
 
 const Tables = () => {
     const [universities, setUni] = useState({ unis: []})
@@ -9,9 +36,13 @@ const Tables = () => {
     useEffect(() => {
         const fetchUniList = async () => {
             const { data } = await axios("http://universities.hipolabs.com/search?country=Australia")
-
+            const output = [...new Map(data.map(o => [o.name, o])).values()]
+            // console.log(data.map(o => [o.name, o]));
+            // console.log(output);
+            // setUni({unis: output})
             setUni({unis: data})
-            console.log(data);
+
+            // console.log(data);
         }
         fetchUniList()
     }, [setUni])
@@ -40,7 +71,38 @@ const Tables = () => {
             <button onClick={loadData}>Load</button>
             <button onClick={addData}>Add</button>
             <button onClick={deleteData}>Delete</button>
-           <table>
+
+            <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>ID</StyledTableCell>
+            <StyledTableCell align="right">Country Code</StyledTableCell>
+            <StyledTableCell align="right">Domains</StyledTableCell>
+            <StyledTableCell align="right">University Name</StyledTableCell>
+            <StyledTableCell align="right">Website</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {visible && universities.unis.map((row, index) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
+                {index+1}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.alpha_two_code}</StyledTableCell>
+              <StyledTableCell align="right"><ul>{row.domains.map((domain) => (
+                  <li>{domain}</li>
+              ))}</ul></StyledTableCell>
+              <StyledTableCell align="right">{row.name}</StyledTableCell>
+              <StyledTableCell align="right"><ul>{row.web_pages.map((web, index) => (
+                  <li>{web}</li>
+              ))}</ul></StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+           {/* <table>
                <thead>
                     <tr>
                         <th>ID</th>
@@ -61,7 +123,9 @@ const Tables = () => {
                         </tr>
                     ))}
                 </tbody>
-           </table>
+           </table> */}
+
+           {/* <MaterialTable columns={columns} data={universities.unis} title="Top University in Australia" /> */}
         </div>
     )
 }
